044df1e7de5134a956a74e2fc0454a8075567e83
Evan C <ecrooks265@gmail.com>
2023-05-07 19:49:31 -0400

    Changed API URL

--- GlobalMarket/projects/trading/src/services/stock-data.service.ts
+++ GlobalMarket/projects/trading/src/services/stock-data.service.ts
@@ -9,7 +9,7 @@ export class StockDataService {
   constructor(private http: HttpClient) { }
 
   predictStock(ticker: string): Observable<any> {
-    const url = `http://127.0.0.1:5000/predict?ticker=${ticker}`;
+    const url = `http://18.118.120.98:5000/predict?ticker=${ticker}`;
     return this.http.get<any>(url);
   }
 }
