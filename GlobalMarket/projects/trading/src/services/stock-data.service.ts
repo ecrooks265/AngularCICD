cd2cc027df65346e48ca6785b01f5a52675121d8
Evan C <ecrooks265@gmail.com>
2023-05-07 20:26:13 -0400

    changed stock predictor back to http get

--- GlobalMarket/projects/trading/src/services/stock-data.service.ts
+++ GlobalMarket/projects/trading/src/services/stock-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -7,18 +7,11 @@ import { Observable } from 'rxjs';
 })
 export class StockDataService {
   constructor(private http: HttpClient) { }
+  apiUrl = 'http://18.118.120.98:5000/predict';
 
-  private apiUrl = 'http://18.118.120.98:5000/predict';
 
   predictStock(ticker: string): Observable<any> {
-    //const url = `http://18.118.120.98:5000/predict?ticker=${ticker}`;
-    //return this.http.get<any>(url);
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
-    const body = { ticker: ticker };
-    return this.http.post<any>(this.apiUrl, body, httpOptions);
+    const url = `${this.apiUrl}?ticker=${ticker}`;
+    return this.http.get<any>(url);
   }
 }
