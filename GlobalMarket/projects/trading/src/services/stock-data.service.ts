a565cc4dc3a23d1814b3b55c67a93a07b15ee935
Evan C <ecrooks265@gmail.com>
2023-05-07 20:13:59 -0400

    Changed api url to a post

--- GlobalMarket/projects/trading/src/services/stock-data.service.ts
+++ GlobalMarket/projects/trading/src/services/stock-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,8 +8,17 @@ import { Observable } from 'rxjs';
 export class StockDataService {
   constructor(private http: HttpClient) { }
 
+  private apiUrl = 'http://18.118.120.98:5000/predict';
+
   predictStock(ticker: string): Observable<any> {
-    const url = `http://18.118.120.98:5000/predict?ticker=${ticker}`;
-    return this.http.get<any>(url);
+    //const url = `http://18.118.120.98:5000/predict?ticker=${ticker}`;
+    //return this.http.get<any>(url);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    const body = { ticker: ticker };
+    return this.http.post<any>(this.apiUrl, body, httpOptions);
   }
 }
