3162ae882a86270205863b59bda59d924fd24a14
Evan C <ecrooks265@gmail.com>
2023-05-07 20:59:16 -0400

    changed proxyconf.js file

--- GlobalMarket/projects/trading/src/proxy.conf.js
+++ GlobalMarket/projects/trading/src/proxy.conf.js
@@ -1,11 +1,11 @@
 const PROXY_CONFIG = [
   {
     context: [
-      "/weatherforecast",
+      "/predict"
     ],
-    target: "https://localhost:7219",
+    target: "http://18.118.120.98:5000",
     secure: false
   }
-]
+];
 
 module.exports = PROXY_CONFIG;
