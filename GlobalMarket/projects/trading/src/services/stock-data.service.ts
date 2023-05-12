761f6b8b02661b40066f658e74dd6a827bbfcced
Evan C <ecrooks265@gmail.com>
2023-05-11 21:06:25 -0400

    added http context to request

--- GlobalMarket/projects/trading/src/services/stock-data.service.ts
+++ GlobalMarket/projects/trading/src/services/stock-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -12,7 +12,8 @@ export class StockDataService {
 
 
   predictStock(ticker: string): Observable<any> {
-    const url =  `http://18.118.120.98:5000${this.apiUrl}?ticker=${ticker}`;
-    return this.http.get<any>(url);
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    const url = `http://18.118.120.98:5000${this.apiUrl}?ticker=${ticker}`;
+    return this.http.get<any>(url, { headers });
   }
 }
