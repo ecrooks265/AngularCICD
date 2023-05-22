2abad3ed01840aacd8018908d168c41e2353129a
Evan C <ecrooks265@gmail.com>
2023-05-21 21:14:56 -0400

    a

--- GlobalMarket/python_files/predictor.py
+++ GlobalMarket/python_files/predictor.py
@@ -35,8 +35,8 @@ def predict_stock(ticker):
     print(df.head())
    
     # Organize data
-    df.index = pd.to_datetime(df["Date"]).dt.tz_convert("US/Central") - dt.timedelta(hours=5)
-    #df.index = pd.to_datetime(df.index, format="%Y-%m-%d", utc = True).tz_convert("US/Central") - dt.timedelta(hours=5)
+    df["Date"] = pd.to_datetime(df["Date"])  # Convert "Date" column to datetime-like format
+    df.index = df["Date"].dt.tz_convert("US/Central") - dt.timedelta(hours=5)
 
     # Print the data frame after modification
     print("After modification:")
@@ -50,7 +50,7 @@ def predict_stock(ticker):
 
     # Set the index as a datetime object
     df.index = pd.to_datetime(df.index)
-     # Create new predictors
+    # Create new predictors
     horizons = [2, 5, 60, 250, 1000]
     new_predictors = []
     for horizon in horizons:
@@ -81,6 +81,7 @@ def predict_stock(ticker):
         y_pred = y_pred_tensor.detach().numpy()
     
     return y_pred[0][0]
+
 
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
