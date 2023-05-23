0ecc4962d936772530ba4b518190c9b43b87c75e
Evan C <ecrooks265@gmail.com>
2023-05-22 20:51:31 -0400

    redo data

--- GlobalMarket/python_files/predictor.py
+++ GlobalMarket/python_files/predictor.py
@@ -22,88 +22,58 @@ def get_data(ticker):
     return stock_data.reset_index()
 
 def predict_stock(ticker):
-       # Load data
-    if ticker + ".csv" in os.listdir("."):
+  try:    # load data
+    if os.path.exists(ticker + ".csv"):
         df = pd.read_csv(ticker + ".csv")
     else:
-        stock = get_data(ticker)
-        df = stock
+        df = yf.Ticker(ticker)
+        df = df.history(period="max")
         df.to_csv(ticker + ".csv")
-    
-    # Print the data frame before modification
-    print("Before modification:")
-    print(df.head())
-    print("Data types before modification:")
-    print(df.dtypes)
 
-    # Convert "Date" column to datetime
-    df["Date"] = pd.to_datetime(df["Date"], infer_datetime_format=True, utc=True)
-
-    # Convert timezone to "US/Central"
-    df["Date"] = df["Date"].dt.tz_convert("US/Central") - pd.Timedelta(hours=5)
-
-    # Print the data frame after modification
-    print("After modification:")
-    print(df.head())
-    print("Data types after modification:")
-    print(df.dtypes)
-
-
-
+    # preprocess data
+    df["Date"] = pd.to_datetime(df["Date"], format='%Y-%m-%d', utc=True).dt.tz_localize(None)
+    df.index = df["Date"].dt.tz_localize(None)
+    df.index = pd.to_datetime(df.index, format="%Y-%m-%d", utc=True).tz_convert("US/Central") - dt.timedelta(hours=5)
+    del df["Date"]
     del df["Dividends"]
     del df["Stock Splits"]
+
     df["Tomorrow"] = df["Close"].shift(-1)
     df["Target"] = (df["Tomorrow"] > df["Close"]).astype(int)
 
-    # Set the index as a datetime object
-    df.index = pd.to_datetime(df.index)
-    # Create new predictors
-    print("Creating new predictors...")
+    df = df.loc[pd.to_datetime("1990-01-01", format='%Y-%m-%d', utc=True).tz_localize(None):].copy()
+
     horizons = [2, 5, 60, 250, 1000]
+
     new_predictors = []
-    try:
 
-      for horizon in horizons:
-          numeric_columns = df.select_dtypes(include=[np.number]).columns
-          rolling_averages = df[numeric_columns].rolling(horizon).mean()
-          ratio_column = f"Close_Ratio_{horizon}"
-          df[ratio_column] = df["Close"] / rolling_averages["Close"]
+    for horizon in horizons:
+        rolling_averages = df.rolling(horizon).mean()
+        ratio_column = f"Close_Ratio_{horizon}"
+        df[ratio_column] = df["Close"] / rolling_averages["Close"]
+        trend_column = f"Trend_{horizon}"
+        df[trend_column] = df.shift(1).rolling(horizon).sum()["Target"]
+        new_predictors += [ratio_column, trend_column]
+    df = df.dropna(subset=df.columns[df.columns != "Tomorrow"])
 
-          print("After applying rolling function:")
-          print(df.head())
-          print("Data types after applying rolling function:")
-          print(df.dtypes)
-
-          trend_column = f"Trend_{horizon}"
-          df[trend_column] = df.shift(1).rolling(horizon).sum()["Target"]
-
-          print("After applying rolling and shifting:")
-          print(df.head())
-          print("Data types after applying rolling and shifting:")
-          print(df.dtypes)
-
-          new_predictors += [ratio_column, trend_column]
-
-      df = df.dropna(subset=df.columns[df.columns != "Tomorrow"])
-      df = df.dropna(subset=["Target"])  # Drop rows with non-numeric values in Target column
     
-      # Load model
-      model = joblib.load("model.pkl")
+    # Load model
+    model = joblib.load("model.pkl")
     
-      # Preprocess data
-      X = df[new_predictors].values.astype('float32')
+    # Preprocess data
+    X = df[new_predictors].values.astype('float32')
     
-      # Make prediction
-      with torch.no_grad():
-          X_tensor = torch.from_numpy(X)
-          y_pred_tensor = model(X_tensor)
-          y_pred = y_pred_tensor.detach().numpy()
+    # Make prediction
+    with torch.no_grad():
+        X_tensor = torch.from_numpy(X)
+        y_pred_tensor = model(X_tensor)
+        y_pred = y_pred_tensor.detach().numpy()
     
-      return y_pred[0][0]
-    except Exception as e:
-      print("Error occurred during predictor creation:")
-      print(e)
-      return None
+    return y_pred[0][0]
+  except Exception as e:
+    print("Error occurred during predictor creation:")
+    print(e)
+    return None
 
 
 @app.route('/predict', methods=['POST', 'GET'])
